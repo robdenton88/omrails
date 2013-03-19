5eeae321a33e37ce346de8cf1d5129b5c1fd61be
Rob Denton <rob@lodgeyourdog.com>
2013-03-19 19:32:26 +0000

    add pins association

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,8 @@
 class Pin < ActiveRecord::Base
   attr_accessible :description
 
-  validates :description, presence: true, #!Find validations under rails validation search
+  validates :description, presence: true #!Find validations under rails validation search
+
+  belongs_to :user
+  validates :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,8 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name
   # attr_accessible :title, :body
+
+  has_many :pins
+# Change to 'has_one' or something to limit to one pin. http://guides.rubyonrails.org/association_basics.html#the-belongs_to-association
+
 end
