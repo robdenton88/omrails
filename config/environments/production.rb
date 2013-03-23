e599eadc3e61098fdff69267bdb86dfb2461b3e1
Rob Denton <rob@lodgeyourdog.com>
2013-03-23 15:13:09 +0000

    add aws piece

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,16 @@ Omrails::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+
+# Paperclip should use amazon S3 on heroku
+config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => "petservicedirectory",
+    :access_key_id => "AKIAI6GKJ3TMDIUP47TA",
+    :secret_access_key => "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"
+  }
+  }
+
 end
